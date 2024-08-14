1aa83ba0d3c55b7f0b4c827c7e821e075c4647db
Suleman Rufai <srufai100@gmail.com>
2024-08-14 13:52:26 +0000

    advanced-tasked

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Setting up my client config file
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
+}
