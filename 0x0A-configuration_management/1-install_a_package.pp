2c40eef32ff94813f5a15955d8d8e1b972ce82c9
Suleman Rufai <srufai100@gmail.com>
2024-08-14 11:31:09 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#install flask with puppet
+# Install Flask 2.1.0 and Werkzeug 2.1.1 using Puppet
 
-package {'flask':
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
