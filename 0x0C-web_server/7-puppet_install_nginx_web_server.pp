ad9a9576f39cb33fd6c8a561031ac9decccbcfe0
Suleman Rufai <srufai100@gmail.com>
2024-08-22 23:10:11 +0000

    web server advance task correction

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,7 +16,7 @@ file_line { 'redirection-301':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/@srufai-Ing permanent;',
 }
 
 service { 'nginx':
